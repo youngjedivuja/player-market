310b000659dddbea92f4c78306797c3aba25dcc3
youngjedivuja <jvujovic1998@gmail.com>
2021-07-03 18:34:47 +0200

    changet dependency for client instead of server

--- transfers/src/main/java/com/example/transfers/config/Config.java
+++ transfers/src/main/java/com/example/transfers/config/Config.java
@@ -1,6 +1,7 @@
 package com.example.transfers.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableJpaAuditing
 public class Config {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
     }
