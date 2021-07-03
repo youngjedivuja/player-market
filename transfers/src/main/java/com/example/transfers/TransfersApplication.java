c3ad09aaeeff75b3d6d85aeed5a1443ee9355795
youngjedivuja <jvujovic1998@gmail.com>
2021-07-03 20:20:39 +0200

    added swagger for player team and transfers projects

--- transfers/src/main/java/com/example/transfers/TransfersApplication.java
+++ transfers/src/main/java/com/example/transfers/TransfersApplication.java
@@ -4,14 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableSwagger2
 public class TransfersApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransfersApplication.class, args);
     }
+
 
 }
