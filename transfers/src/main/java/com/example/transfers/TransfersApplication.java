b8fd11b89fba8e555f1fff2b489a3d4c78dedb28
youngjedivuja <jvujovic1998@gmail.com>
2021-07-03 19:34:24 +0200

    Fixed yml files to enable routing from api gateway

--- transfers/src/main/java/com/example/transfers/TransfersApplication.java
+++ transfers/src/main/java/com/example/transfers/TransfersApplication.java
@@ -3,10 +3,12 @@ package com.example.transfers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class TransfersApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransfersApplication.class, args);
