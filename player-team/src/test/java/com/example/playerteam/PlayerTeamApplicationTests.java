3491ae86e6d561b6d1b9957acc8c6ebb143dcf96
youngjedivuja <jvujovic1998@gmail.com>
2021-07-03 10:23:16 +0200

    player-team blank project

---
+++ player-team/src/test/java/com/example/playerteam/PlayerTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playerteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
