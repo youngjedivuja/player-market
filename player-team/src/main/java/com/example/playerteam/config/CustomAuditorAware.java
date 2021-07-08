06122656dc5fe957360dfce0b3eeb0ce4923ec23
youngjedivuja <jvujovic1998@gmail.com>
2021-07-08 18:28:22 +0200

    Added config for jpa auditing

---
+++ player-team/src/main/java/com/example/playerteam/config/CustomAuditorAware.java
@@ -0,0 +1,14 @@
+package com.example.playerteam.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CustomAuditorAware implements AuditorAware<String> {
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		return Optional.of("system");
+	}
+}
